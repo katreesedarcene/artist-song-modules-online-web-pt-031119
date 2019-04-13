8b8f4d33764c35b52fd3b94eeefc271775162a95
Kat B <katreesebarnes@gmail.com>
2019-04-13 13:37:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require_relative '../lib/artist'
 class Artist
   attr_accessor :name
   attr_reader :songs
+
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
@@ -11,7 +12,7 @@ class Artist
   @@artists = []
 
   def initialize
- super
+    @@artists << self
     @songs = []
   end
 
@@ -23,18 +24,13 @@ class Artist
     @@artists
   end
 
- 
-
-def add_song(song)
+  def add_song(song)
     @songs << song
     song.artist = self unless song.artist
   end
 
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+
 end
