8232555ee7e883f24ee1c1b439e00c5c216c1477
Kat B <katreesebarnes@gmail.com>
2019-04-13 15:00:40 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,7 +8,11 @@ module Memorable
     def count
       self.all.count
     end
-    
+     def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
+
   end
   
   module InstanceMethods
