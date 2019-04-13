e489fc5138be3cb4eff0ec1cc593939fa4825c10
Kat B <katreesebarnes@gmail.com>
2019-04-13 14:45:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,13 +20,13 @@ class Artist
     @@artists
   end
 
-  def add_song(song)
-    # @songs << song
-    song.artist = self unless song.artist
-  end
+  # def add_song(song)
+  #   # @songs << song
+  #   song.artist = self unless song.artist
+  # end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+  # def add_songs(songs)
+  #   songs.each { |song| add_song(song) }
+  # end
 
 end
